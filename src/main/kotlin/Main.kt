76c91cbec433192d4e0e3b32fe52d98f5e55aa79
sehwan-son <sehwan.son@webtoontoday.com>
2023-11-17 01:15:35 +0900

    Introduction

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,5 +7,3 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
-
-//git test
